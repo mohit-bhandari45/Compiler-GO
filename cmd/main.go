7bc1c1b52ea3cd71ce66269014b4eb435323bef9
mohit-bhandari45 <mohitbhandari852@gmail.com>
2025-05-23 13:34:03 +0530

    added lexical analyzer

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mohit-bhandari45/Compiler-GO/lexer"
+)
+
+func main() {
+	input := `let five = 5;`
+
+	l := lexer.New(input);
+	for tok := l.NextToken(); tok.Type!=lexer.EOF; tok = l.NextToken() {
+		fmt.Printf("%+v\n", tok);
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mohit-bhandari45/Compiler-GO
+
+go 1.24.0
